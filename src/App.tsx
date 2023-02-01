53565acff3cddb7fdb5f20f6fce0cce84b8a17c1
Muratov Ivan <ivanmuratov121096@gmail.com>
2023-02-01 12:21:23 +0700

    add react-router-dom, pages/*

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { FC } from "react";
-import Counter from "./components/Counter";
+import { FC, Suspense } from "react";
 import './App.scss';
+import { Link, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage.async";
+import AboutPage from "./pages/AboutPage/AboutPage.async";
 
 const App: FC = () => {
     return ( 
         <div className='app'>
-            asdasdas
-            <Counter />
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>О странице</Link>
+            <Suspense fallback={<div>... loading</div>}>
+                <Routes>                
+                    <Route index element={<MainPage />} />
+                    <Route path='/about' element={<AboutPage />} />                        
+                </Routes>
+            </Suspense>       
         </div>
     );
 };
