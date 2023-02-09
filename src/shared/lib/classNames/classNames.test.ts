fa6f3be3d602a23544e16b99376a90d6a0ed4280
Muratov Ivan <ivanmuratov121096@gmail.com>
2023-02-09 15:47:14 +0700

    add jest; add test shared/lib/classNames

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,27 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  it('with one param', () => {
+    expect(classNames('class')).toBe('class');
+  });
+
+  it('with additional', () => {
+    const expected = 'class class1 class2';
+    expect(classNames('class', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  it('with mods', () => {
+    const expected = 'class class1 class2 foo bar';
+    expect(classNames('class', { foo: true, bar: true }, ['class1', 'class2'])).toBe(expected);
+  });
+
+  it('with mods false', () => {
+    const expected = 'class class1 class2 foo';
+    expect(classNames('class', { foo: true, bar: false }, ['class1', 'class2'])).toBe(expected);
+  });
+
+  it('with mods undefined', () => {
+    const expected = 'class class1 class2 foo';
+    expect(classNames('class', { foo: true, bar: undefined }, ['class1', 'class2'])).toBe(expected);
+  });
+});
