925f559bdad868e1b4ebc17bc4928947601f7812
Muratov Ivan <ivanmuratov121096@gmail.com>
2023-03-30 14:58:44 +0700

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,12 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/const/localStorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    // eslint-disable-next-line no-param-reassign
+    config.headers.authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+  }
+  return config;
 });
