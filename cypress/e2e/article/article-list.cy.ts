9215d331bdd079311a7b9de3aa57ade8d74705ff
Muratov Ivan <ivanmuratov121096@gmail.com>
2023-05-03 16:01:32 +0700

    add fixtures

--- cypress/e2e/article/article-list.cy.ts
+++ cypress/e2e/article/article-list.cy.ts
@@ -9,6 +9,12 @@ describe('User visits the article list page', () => {
     cy.getByTestId('ArticleList').should('exist');
     cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 1);
   });
+
+  it('Fixtures. Article list loading successfully', () => {
+    cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
+    cy.getByTestId('ArticleList').should('exist');
+    cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 1);
+  });
 });
 
 export {};
