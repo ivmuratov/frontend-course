4ba74309d38c9bbc85820523e7fc8fc42761aa55
Muratov Ivan <ivanmuratov121096@gmail.com>
2023-05-05 14:31:28 +0700

    add getApiUrl

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,18 @@
 import path from 'path';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
+
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+  if (apiUrl) {
+    return apiUrl;
+  }
+
+  if (mode === 'production') {
+    return '/api';
+  }
+
+  return 'http://localhost:8080';
+}
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
@@ -16,7 +28,7 @@ export default (env: BuildEnv) => {
   const analyze = env.analyze || false;
   const ANALYZE_PORT = env.analyzePort || 8888;
   const isDev = mode === 'development';
-  const apiUrl = env.apiUrl || 'http://localhost:8080';
+  const apiUrl = getApiUrl(mode, env?.apiUrl);
   const PORT = env.port || 3000;
 
   return buildWebpackConfig({
